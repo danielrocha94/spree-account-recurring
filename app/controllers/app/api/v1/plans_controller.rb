ae8574581c08f899b50abcb40745fdc625e57ff3
Ubuntu <ubuntu@ip-172-31-25-221.us-east-2.compute.internal>
2021-10-14 22:30:47 -0500

    add API Endpoint to plans controller

---
+++ app/controllers/app/api/v1/plans_controller.rb
@@ -0,0 +1,21 @@
+require 'base64'
+class App::Api::V1::PlansController < App::Api::V1::ApplicationController
+  before_action :load_user_subscriptions
+
+  def index
+    plans = Spree::Plan.visible.order('id desc')
+    render :json => {
+      plans: plans,
+      user_subscriptions: @user_subscriptions
+    }
+  end
+
+  private
+    def load_user_subscriptions
+      if spree_current_user
+        @user_subscriptions = spree_current_user.subscription_plans.undeleted.all.to_a
+      else
+        @user_subscriptions = []
+      end
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,32 @@
-Spree::Core::Engine.routes.draw do
-  namespace :admin do
-    resources :recurrings, except: :show do
-      resources :plans, except: :show
+Rails.application.routes.draw do 
+  Spree::Core::Engine.routes.draw do
+    namespace :admin do
+      resources :recurrings, except: :show do
+        resources :plans, except: :show
+      end
+
+      resources :subscription_plans, only: :index
+      resources :subscription_events, only: :index
     end
 
-    resources :subscription_plans, only: :index
-    resources :subscription_events, only: :index
+    resources :recurring_hooks, only: :none do
+      post :handler, on: :collection
+    end
+
+    resources :plans, only: :index, controller: :plans do
+      resources :subscription_plans, only: [:show, :create, :destroy, :new]
+    end
+
   end
 
-  resources :recurring_hooks, only: :none do
-    post :handler, on: :collection
-  end
-
-  resources :plans, only: :index, controller: :plans do
-    resources :subscription_plans, only: [:show, :create, :destroy, :new]
+  scope "(:locale)", locale: /en|es/ do
+    namespace :app do
+      namespace :api, defaults: {format: :json} do
+        namespace :v1 do
+          get 'plans' => 'plans#index';
+        end
+      end
+    end
   end
 end
+
