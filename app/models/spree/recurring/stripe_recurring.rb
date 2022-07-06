08a44af18b5730e71bd41fe202e59ed24b8db9e3
Daniel Rocha <danielrocha94@msn.com>
2022-07-06 14:31:49 -0500

    Add daily option to stripe recurring plan

--- app/models/spree/recurring/stripe_recurring.rb
+++ app/models/spree/recurring/stripe_recurring.rb
@@ -5,7 +5,7 @@ module Spree
 
       WEBHOOKS = ['customer.subscription.deleted', 'customer.subscription.created', 'customer.subscription.updated', 'invoice.payment_succeeded', 'invoice.payment_failed', 'charge.succeeded', 'charge.failed', 'charge.refunded', 'charge.captured', 'plan.created', 'plan.updated', 'plan.deleted']
 
-      INTERVAL = { week: 'Weekly', month: 'Monthly', year: 'Annually' }
+      INTERVAL = { day: 'Daily', week: 'Weekly', month: 'Monthly', year: 'Annually' }
       CURRENCY = { usd: 'USD', mxn: 'MXN', gbp: 'GBP', jpy: 'JPY', eur: 'EUR', aud: 'AUD', hkd: 'HKD', sek: 'SEK', nok: 'NOK', dkk: 'DKK', pen: 'PEN', cad: 'CAD'}
 
       after_initialize :set_api_key
--- spec/models/spree/recurring/stripe_recurring_spec.rb
+++ spec/models/spree/recurring/stripe_recurring_spec.rb
@@ -4,7 +4,7 @@ describe Spree::Recurring::StripeRecurring do
   let(:stripe_recurring) { Spree::Recurring::StripeRecurring.create!(name: 'Test recurring', active: true, preferred_secret_key: 'test_secret_key') }
 
   it { Spree::Recurring::StripeRecurring::WEBHOOKS.should eq(['customer.subscription.deleted', 'customer.subscription.created', 'customer.subscription.updated', 'invoice.payment_succeeded', 'invoice.payment_failed', 'charge.succeeded', 'charge.failed', 'charge.refunded', 'charge.captured', 'plan.created', 'plan.updated', 'plan.deleted']) }
-  it { Spree::Recurring::StripeRecurring::INTERVAL.should eq({ week: 'Weekly', month: 'Monthly', year: 'Annually' }) }
+  it { Spree::Recurring::StripeRecurring::INTERVAL.should eq({day: 'Daily', week: 'Weekly', month: 'Monthly', year: 'Annually' }) }
   it { Spree::Recurring::StripeRecurring::CURRENCY.should eq({ usd: 'USD', gbp: 'GBP', jpy: 'JPY', eur: 'EUR', aud: 'AUD', hkd: 'HKD', sek: 'SEK', nok: 'NOK', dkk: 'DKK', pen: 'PEN', cad: 'CAD'})}
 
   describe '#after_initialize set_api_key' do
